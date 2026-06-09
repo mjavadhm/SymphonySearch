9b2ff61cf2c40d77cede3dfb752372fe9d22e015
Javad <you@example.com>
2026-06-10 01:47:01 +0330

    Fix: Add legacy-kapt with AGP version for ObjectBox code generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,5 +13,5 @@ plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.compose.compiler) apply false
   alias(libs.plugins.kotlin.serialization) apply false
-
+  alias(libs.plugins.android.legacy.kapt) apply false
 }
